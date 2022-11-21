ac8e8f3a19292fc3f26933b96204fb8d8904c6dd
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-11-21 10:36:25 +0200

    add mock function

--- test/cubit/game_cubit_test.dart
+++ test/cubit/game_cubit_test.dart
@@ -1,15 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:puzzle_app/core/service/common_functions.dart';
 import 'package:puzzle_app/core/service/navigator.dart';
 import 'package:puzzle_app/features/game/cubit/game_cubit.dart';
 import 'package:puzzle_app/routes/app_router.dart';
 
 void main() {
   final router = AppRouter();
+  final allFunc = CommonFunctions(); // !!! make mock!!!
 
   late final GameCubit sut;
   setUp(() {
     sut = GameCubit(
       PuzzleNavigator(router),
+      allFunc,
     );
   });
 
