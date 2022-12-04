f1b6aeb76f04d24bdbf2485d37d13a8a1f5a716f
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-12-04 10:04:01 +0200

    add test if puzzle complited

--- test/cubit/game_cubit_test.dart
+++ test/cubit/game_cubit_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:puzzle_app/core/model/box.dart';
 import 'package:puzzle_app/core/service/common_functions.dart';
 import 'package:puzzle_app/core/service/navigator.dart';
 import 'package:puzzle_app/features/game/cubit/game_cubit.dart';
@@ -6,7 +7,7 @@ import 'package:puzzle_app/routes/app_router.dart';
 
 void main() {
   final router = AppRouter();
-  final allFunc = CommonFunctions(); // !!! make mock!!!
+  final allFunc = CommonFunctions();
 
   late final GameCubit sut;
   setUp(() {
@@ -18,11 +19,30 @@ void main() {
 
   group('Game cubit tests', () {
     test('init updates state with list of numbers', () async {
-      // Arrange (Given)
-      // Act (When)
       sut.init();
-      // Assert (Then)
       expect(sut.state.numbers.isEmpty, isFalse);
+    });
+    test('init updates state with list of coords', () async {
+      sut
+        ..init()
+        ..fillInitialCoordList();
+      expect(sut.state.listBoxes.isEmpty, isFalse);
+    });
+    test('updates steps count after correct step', () async {
+      sut.init();
+      //final steps = sut.state.stepsCount;
+      //sut.swapBoxes(13, 50, 0);
+      //expect(sut.state.stepsCount, steps + 1);
+    });
+    test('puzzle completed', () async {
+      final list1 = [
+        BoxWithCoord(coordX: 0, coordY: 0, text: 1),
+      ];
+      final list2 = [
+        BoxWithCoord(coordX: 0, coordY: 0, text: 1),
+      ];
+      sut.isPuzzleCompleted(list1, list2);
+      expect(sut.state.playerWin, true);
     });
   });
 }
