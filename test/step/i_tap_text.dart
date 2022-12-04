59edfb3bfa52f2bbd7747bd82f0aa6a968d2058d
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-12-04 10:02:19 +0200

    update function for tap text

--- test/step/i_tap_text.dart
+++ test/step/i_tap_text.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 /// Example: When I tap {'some'} text
@@ -5,6 +6,12 @@ Future<void> iTapText(
   WidgetTester tester,
   String text,
 ) async {
-  await tester.tap(find.text(text));
-  await tester.pump();
+  //await tester.tap(find.text(text));
+  //await tester.pump();
+
+  final key = 'box$text';
+  await tester.ensureVisible(find.byKey(Key(key)));
+
+  await tester.tap(find.byKey(Key(key)));
+  await tester.pumpAndSettle();
 }
